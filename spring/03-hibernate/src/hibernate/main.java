7e389beb71af16396e0ad12ccbf4bf3834e312b2
KennyBlanckaert <KennyBlanckaert@hotmail.com>
2019-02-12 20:04:53 +0100

    Read objects + log4j low-level tracing

--- spring/03-hibernate/src/hibernate/main.java
+++ spring/03-hibernate/src/hibernate/main.java
@@ -1,5 +1,7 @@
 package hibernate;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -18,16 +20,26 @@ public class main {
 		Session session = factory.getCurrentSession();
 		
 		try {
-			System.out.println("Start!");
 			Student student = new Student("hibernate", "student");
-			System.out.println("1!");
+			
+			// Write
 			session.beginTransaction();
-			System.out.println("2!");
 			session.save(student);
-			System.out.println("3!");
 			session.getTransaction().commit();
 			
+			// Read examples
+			session.createQuery("FROM students").list();
+			session.createQuery("FROM students s WHERE s.firstname LIKE 'Mon%'").list();
+			session.createQuery("FROM students s WHERE s.lastname = 'Luffy'").list();
+			
 			System.out.println("Done!");
+			
+			/* PS:
+			 * 		queries are show on stdout because of the show_sql property in the hibernate.cfg.xml file
+			 * 		anyway, when adding an object to the database, this doesn't print the actual values but question marks.
+			 * 		To actually see the added values, you can make use of log4j (see the .properties file).
+			 * 		This configuration file contains the TRACE option for low-level tracing
+			 * */
 		}
 		catch(Exception e) {
 			e.printStackTrace();

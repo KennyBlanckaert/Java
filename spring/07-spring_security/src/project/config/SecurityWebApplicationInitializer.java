92ad9238aa69e06be8b47a3fa44ebc7a508699ff
KennyBlanckaert <KennyBlanckaert@hotmail.com>
2019-02-17 12:38:02 +0100

    Custom login page

--- spring/07-spring_security/src/project/config/SecurityWebApplicationInitializer.java
+++ spring/07-spring_security/src/project/config/SecurityWebApplicationInitializer.java
@@ -2,6 +2,7 @@ package project.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+// Enables Spring Security Filters
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
 
 }
